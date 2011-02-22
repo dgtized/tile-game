0102ae0e8bfa352088c991c6d0a22794459fdf63
Charles Comstock <dgtized@gmail.com>
2011-02-22 16:23:10 -0600

    upgraded to snapshot of swank-clojure

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A Tile Ordering Game and Solver"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
