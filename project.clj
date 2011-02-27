ac77fd52777808433e63ae474e22943c3846b358
Charles Comstock <dgtized@gmail.com>
2011-02-27 15:01:07 -0600

    updated README and project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tile-game "1.0.0-SNAPSHOT"
-  :description "A Tile Ordering Game and Solver"
+  :description "A Tile Puzzle Game and Solver"
   :main tile-game.core
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
