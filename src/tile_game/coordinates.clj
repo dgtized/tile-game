6783907a1c1f778b63ea81e046fc1bac2a9c8e55
Charles Comstock <dgtized@gmail.com>
2013-11-06 16:50:32 -0600

    Improve decoupling and ensuring clockwise order of NESW

--- src/tile_game/coordinates.clj
+++ src/tile_game/coordinates.clj
@@ -1,29 +1,40 @@
 (ns tile-game.coordinates
-  (require [tile-game.board :refer [distance adjacent-coords]]))
+  (require [tile-game.board :refer [distance]]))
 
-(defn closest-adjacent-coordinate
+(defn adjacent-to
+  "Lists adjacent coordinates in clockwise order"
+  [[x y]]
+  (map (fn [[dx dy]] [(+ x dx) (+ y dy)])
+       [[0 -1] [1 0] [0 1] [-1 0]]))
+
+(defn closest-adjacent
   "Given two points p1, p2 in the form [x y], return coordinate
   adjacent to p1 that is closest to p2"
-  [board p1 p2]
+  [p1 p2]
   (if (= p1 p2)
     p2
     (apply min-key
            (partial distance p2)
-           (adjacent-coords board p1))))
+           (adjacent-to p1))))
 
-(defn path-to [board p1 p2]
+(defn path-to
+  "Shortest path from p1 to p2"
+  [p1 p2]
   (take (distance p1 p2)
-        (drop 1 (iterate #(closest-adjacent-coordinate board % p2) p1))))
+        (drop 1 (iterate #(closest-adjacent % p2) p1))))
 
-(defn circumfrence [board p1 p2]
+(defn circumfrence
+  "Lists coordinates on the circumfrence of largest rectangle
+  described by p1, p2 in clockwise order"
+  [p1 p2]
   (let [xs    (map first [p1 p2])
         ys    (map second [p1 p2])
         min-x (apply min xs)
         max-x (apply max xs)
         min-y (apply min ys)
         max-y (apply max ys)]
-    (concat (path-to board [min-x min-y] [max-x min-y])
-            (path-to board [max-x min-y] [max-x max-y])
-            (path-to board [max-x max-y] [min-x max-y])
-            (path-to board [min-x max-y] [min-x min-y]))))
+    (concat (path-to [min-x min-y] [max-x min-y])
+            (path-to [max-x min-y] [max-x max-y])
+            (path-to [max-x max-y] [min-x max-y])
+            (path-to [min-x max-y] [min-x min-y]))))
 
--- test/tile_game/coordinates_test.clj
+++ test/tile_game/coordinates_test.clj
@@ -2,29 +2,30 @@
   (:require [tile-game.coordinates :refer :all]
             [clojure.test :refer :all]))
 
-(def a-board (tile-game.board/create-board 3))
+(deftest adjacency-list
+  (is (= (adjacent-to [1 1]) '([1 0] [2 1] [1 2] [0 1]))))
 
 (deftest circumfrence-coordinates
-  (is (= (circumfrence a-board [0 0] [1 1])
+  (is (= (circumfrence [0 0] [1 1])
          '([1 0] [1 1] [0 1] [0 0])))
-  (is (= (circumfrence a-board [0 0] [2 0])
+  (is (= (circumfrence [0 0] [2 0])
          '([1 0] [2 0] [1 0] [0 0])))
-  (is (= (circumfrence a-board [0 0] [0 1])
+  (is (= (circumfrence [0 0] [0 1])
          '([0 1] [0 0])))
-  (is (= (circumfrence a-board [0 0] [2 2])
+  (is (= (circumfrence [0 0] [2 2])
          '([1 0] [2 0]
              [2 1] [2 2]
                [1 2] [0 2]
                  [0 1] [0 0]))))
 
 (deftest closest-adjacent-coordinates
-  (is (= (closest-adjacent-coordinate a-board [0 0] [0 0]) [0 0]))
-  (is (= (closest-adjacent-coordinate a-board [0 0] [0 1]) [0 1]))
-  (is (= (closest-adjacent-coordinate a-board [0 0] [0 2]) [0 1]))
-  (is (= (closest-adjacent-coordinate a-board [0 0] [1 1]) [0 1]))
-  (is (= (closest-adjacent-coordinate a-board [0 0] [1 0]) [1 0])))
+  (is (= (closest-adjacent [0 0] [0 0]) [0 0]))
+  (is (= (closest-adjacent [0 0] [0 1]) [0 1]))
+  (is (= (closest-adjacent [0 0] [0 2]) [0 1]))
+  (is (= (closest-adjacent [0 0] [1 1]) [0 1]))
+  (is (= (closest-adjacent [0 0] [1 0]) [1 0])))
 
 (deftest paths-to
-  (is (= (path-to a-board [0 0] [0 0]) '()))
-  (is (= (path-to a-board [0 0] [0 1]) '([0 1])))
-  (is (= (path-to a-board [0 0] [0 2]) '([0 1] [0 2]))))
+  (is (= (path-to [0 0] [0 0]) '()))
+  (is (= (path-to [0 0] [0 1]) '([0 1])))
+  (is (= (path-to [0 0] [0 2]) '([0 1] [0 2]))))
