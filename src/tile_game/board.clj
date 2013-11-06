4b0c5ba94e56bf995a277a974dc269fff6cfec84
Charles Comstock <dgtized@gmail.com>
2013-11-06 16:05:26 -0600

    Introduce a path-to for building ordered circumfrence

--- src/tile_game/board.clj
+++ src/tile_game/board.clj
@@ -173,9 +173,28 @@
     (let [next (+ 1 (count (solved-tiles board)))]
       (solve-tile board next))))
 
-(defn circumfrence [board [x0 y0] [x1 y1]]
-  (for [y (range y0 (inc y1))
-        x (range x0 (inc x1))
-        :when (or (= x x0) (= x x1)
-                  (= y y0) (= y y1)) ]
-    [x y]))
+(defn closest-adjacent-coordinate
+  "Given two points p1, p2 in the form [x y], return coordinate
+  adjacent to p1 that is closest to p2"
+  [board p1 p2]
+  (if (= p1 p2)
+    p2
+    (apply min-key
+           (partial distance p2)
+           (adjacent-coords board p1))))
+
+(defn path-to [board p1 p2]
+  (take (distance p1 p2)
+        (drop 1 (iterate #(closest-adjacent-coordinate board % p2) p1))))
+
+(defn circumfrence [board p1 p2]
+  (let [xs    (map first [p1 p2])
+        ys    (map second [p1 p2])
+        min-x (apply min xs)
+        max-x (apply max xs)
+        min-y (apply min ys)
+        max-y (apply max ys)]
+    (concat (path-to board [min-x min-y] [max-x min-y])
+            (path-to board [max-x min-y] [max-x max-y])
+            (path-to board [max-x max-y] [min-x max-y])
+            (path-to board [min-x max-y] [min-x min-y]))))
--- test/tile_game/board_test.clj
+++ test/tile_game/board_test.clj
@@ -98,12 +98,25 @@
 
 (deftest circumfrence-coordinates
   (is (= (circumfrence a-board [0 0] [1 1])
-         '([0 0] [1 0] [0 1] [1 1])))
+         '([1 0] [1 1] [0 1] [0 0])))
   (is (= (circumfrence a-board [0 0] [2 0])
-         '([0 0] [1 0] [2 0])))
+         '([1 0] [2 0] [1 0] [0 0])))
   (is (= (circumfrence a-board [0 0] [0 1])
-         '([0 0] [0 1])))
+         '([0 1] [0 0])))
   (is (= (circumfrence a-board [0 0] [2 2])
-         '([0 0] [1 0] [2 0]
-           [0 1] [2 1]
-           [0 2] [1 2] [2 2]))))
+         '([1 0] [2 0]
+             [2 1] [2 2]
+               [1 2] [0 2]
+                 [0 1] [0 0]))))
+
+(deftest closest-adjacent-coordinates
+  (is (= (closest-adjacent-coordinate a-board [0 0] [0 0]) [0 0]))
+  (is (= (closest-adjacent-coordinate a-board [0 0] [0 1]) [0 1]))
+  (is (= (closest-adjacent-coordinate a-board [0 0] [0 2]) [0 1]))
+  (is (= (closest-adjacent-coordinate a-board [0 0] [1 1]) [0 1]))
+  (is (= (closest-adjacent-coordinate a-board [0 0] [1 0]) [1 0])))
+
+(deftest paths-to
+  (is (= (path-to a-board [0 0] [0 0]) '()))
+  (is (= (path-to a-board [0 0] [0 1]) '([0 1])))
+  (is (= (path-to a-board [0 0] [0 2]) '([0 1] [0 2]))))
