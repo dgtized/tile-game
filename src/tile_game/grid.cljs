78714a1174c6d99ea5101b43eb76d16bf61c8238
Charles Comstock <dgtized@gmail.com>
2017-03-12 03:42:37 -0500

    verify color sequence works for larger grids

--- src/tile_game/grid.cljs
+++ src/tile_game/grid.cljs
@@ -4,18 +4,19 @@
 
 (enable-console-print!)
 
-(def colors ["#000000" "#808080" "#C0C0C0" "#FFFFFF"
-             "#800000" "#FF0000" "#808000" "#FFFF00"
-             "#008000" "#00FF00" "#008080" "#00FFFF"
-             "#000080" "#0000FF" "#800080" "#FF00FF"])
+(def colors
+  (cycle ["#00B000" "#808080" "#C0C0C0" "#3030B0"
+          "#800000" "#FF0000" "#808000" "#FFFF00"
+          "#008000" "#00FF00" "#008080" "#00FFFF"
+          "#0000C0" "#3030FF" "#800080" "#FF00FF"]))
 
 (defonce app-state
-  (r/atom {:board (b/create-board 4)
-           :dim 4}))
+  (r/atom {:board (b/create-board 5)
+           :dim 5}))
 
 (defn render-tile [[x y] tile]
   (if-not (zero? tile)
-    (let [fill-color (colors tile)]
+    (let [fill-color (nth colors tile)]
       [:g {:key (str "tile-" tile)
            :on-click #(swap! app-state update-in [:board] b/slide-tile tile)}
        [:rect {:x (+ x 0.05) :y (+ y 0.05) :width 0.9 :height 0.9 :fill fill-color}]
@@ -27,7 +28,7 @@
   (let [{:keys [board dim]} @app-state]
     [:center
      [:h1 "Tile Grid"]
-     [:svg {:view-box "0 0 4 4" :width 800 :height 800}
+     [:svg {:view-box (str "0 0 " dim " " dim) :width 800 :height 800}
       (for [x (range dim) y (range dim)]
         (let [tile (nth board (+ (* y dim) x))]
           (render-tile [x y] tile)))]]))
