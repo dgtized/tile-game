31c0839010256313d5b9b4f4dca768775a10c486
Charles Comstock <dgtized@gmail.com>
2017-03-12 03:23:38 -0500

    Re-use vector create-board & allow on-click to move tiles

--- src/tile_game/grid.cljs
+++ src/tile_game/grid.cljs
@@ -1,5 +1,6 @@
 (ns tile-game.grid
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [tile-game.board :as b]))
 
 (enable-console-print!)
 
@@ -9,13 +10,14 @@
              "#000080" "#0000FF" "#800080" "#FF00FF"])
 
 (defonce app-state
-  (r/atom {:board (range 0 16)
+  (r/atom {:board (b/create-board 4)
            :dim 4}))
 
 (defn render-tile [[x y] tile]
   (if-not (zero? tile)
     (let [fill-color (colors tile)]
-      [:g {:key (str "tile-" tile) :on-click #(println tile)}
+      [:g {:key (str "tile-" tile)
+           :on-click #(swap! app-state update-in [:board] b/slide-tile tile)}
        [:rect {:x (+ x 0.05) :y (+ y 0.05) :width 0.9 :height 0.9 :fill fill-color}]
        [:text {:x (+ 0.5 x) :y (+ 0.65 y)
                :font-family "Verdana" :font-size 0.4 :text-anchor "middle"}
