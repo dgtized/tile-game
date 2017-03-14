9b3ca310a0c1192635e5e101eb70c8f2332d0b4e
Charles Comstock <dgtized@gmail.com>
2017-03-13 23:33:39 -0500

    Reformat cljsbuild & fighwheel config and specify more

--- project.clj
+++ project.clj
@@ -13,21 +13,22 @@
    "resources/public/js/release"
    "resources/public/js/tile-game.js"
    :target-path]
-  :cljsbuild
-  { :builds [{:id "dev"
-              :source-paths ["src"]
-              :figwheel true
-              :compiler {:main tile-game.grid
-                         :asset-path "js/out"
-                         :output-to "resources/public/js/tile-game.js"
-                         :output-dir "resources/public/js/out"
-                         :source-map-timestamp true}}
-             {:id "release"
-              :source-paths ["src"]
-              :compiler {:main tile-game.grid
-                         :output-to "resources/public/js/tile-game.js"
-                         :output-dir "resources/public/js/release"
-                         :optimizations :advanced
-                         :source-map "resources/public/js/tile-game.js.map"}}]}
-  :figwheel { :css-dirs ["resources/public/css"]
-              :open-file-command "emacsclient" })
+  :cljsbuild {:builds
+              {"dev"
+               {:source-paths ["src"]
+                :figwheel {}
+                :compiler {:main tile-game.grid
+                           :asset-path "js/out"
+                           :output-to "resources/public/js/tile-game.js"
+                           :output-dir "resources/public/js/out"
+                           :optimizations :none
+                           :source-map-timestamp true}}
+               "release"
+               {:source-paths ["src"]
+                :compiler {:main tile-game.grid
+                           :output-to "resources/public/js/tile-game.js"
+                           :output-dir "resources/public/js/release"
+                           :optimizations :advanced
+                           :source-map "resources/public/js/tile-game.js.map"}}}}
+  :figwheel {:css-dirs ["resources/public/css"]
+             :open-file-command "emacsclient"})
