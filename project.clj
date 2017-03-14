ca72c7aa26a0b5f9ba34ca25319c22cdb43a14ba
Charles Comstock <dgtized@gmail.com>
2017-03-13 22:20:14 -0500

    Add basic cljsbuild release build

--- project.clj
+++ project.clj
@@ -5,20 +5,29 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.494"]
                  [reagent "0.6.1"]]
-  :plugins [[lein-figwheel "0.5.9"]]
+  :plugins [[lein-figwheel "0.5.9"]
+            [lein-cljsbuild "1.1.5"]]
   :sources-paths ["src"]
   :clean-targets ^{:protect false}
   ["resources/public/js/out"
+   "resources/public/js/release"
    "resources/public/js/tile-game.js"
    :target-path]
   :cljsbuild
-  { :builds [{:id "tile-game"
+  { :builds [{:id "dev"
               :source-paths ["src"]
               :figwheel true
-              :compiler {:main "tile-game.grid"
+              :compiler {:main tile-game.grid
                          :asset-path "js/out"
                          :output-to "resources/public/js/tile-game.js"
                          :output-dir "resources/public/js/out"
-                         :source-map-timestamp true}}]}
+                         :source-map-timestamp true}}
+             {:id "release"
+              :source-paths ["src"]
+              :compiler {:main tile-game.grid
+                         :output-to "resources/public/js/tile-game.js"
+                         :output-dir "resources/public/js/release"
+                         :optimizations :advanced
+                         :source-map "resources/public/js/tile-game.js.map"}}]}
   :figwheel { :css-dirs ["resources/public/css"]
               :open-file-command "emacsclient" })
