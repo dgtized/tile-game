6ffddf0ae018568c1a98f2a5ee18146a70575adf
Charles Comstock <dgtized@gmail.com>
2017-03-17 13:58:59 -0500

    increase safety of publish by forcing the git repo to clear before creating it

--- publish.sh
+++ publish.sh
@@ -3,10 +3,10 @@
 lein clean
 lein cljsbuild once release
 
+rm -rf resources/public/.git
 pushd resources/public
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "git@github.com:dgtized/tile-game.git" master:gh-pages
 popd
-rm -rf resources/public/.git
