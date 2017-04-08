83df17b46993c1f8c48b166394b9b7c3206be3d6
Charles Comstock <dgtized@gmail.com>
2017-04-07 19:53:05 -0500

    Normalize config like sierpinski
    Removes need for special setup around cljsbuild/figwheel hosted in cider

--- project.clj
+++ project.clj
@@ -3,15 +3,16 @@
   :min-lein-version "2.0.0"
   :main tile-game.core
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [org.clojure/core.async "0.3.441"]
+                 [org.clojure/clojurescript "1.9.495"]
+                 [org.clojure/core.async "0.3.442"]
                  [reagent "0.6.1"]]
   :plugins [[lein-figwheel "0.5.9"]
-            [lein-cljsbuild "1.1.5"]]
+            [lein-cljsbuild "1.1.5" :exclusions [[org.clojure/clojure]]]]
   :sources-paths ["src"]
-  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
-                                  [figwheel-sidecar "0.5.8"]]
-                   :source-paths ["cljs_src" "dev"] }}
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.2"]
+                                  [com.cemerick/piggieback "0.2.1"]
+                                  [figwheel-sidecar "0.5.9"]]
+                   :source-paths ["src" "dev"] }}
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :clean-targets ^{:protect false}
   ["resources/public/js/out"
@@ -21,13 +22,15 @@
   :cljsbuild {:builds
               {"dev"
                {:source-paths ["src"]
-                :figwheel {}
+                :figwheel {:on-jsload "tile-game.grid/on-js-reload",
+                           :open-urls ["http://localhost:3449/index.html"]}
                 :compiler {:main tile-game.grid
                            :asset-path "js/out"
                            :output-to "resources/public/js/tile-game.js"
                            :output-dir "resources/public/js/out"
                            :optimizations :none
-                           :source-map-timestamp true}}
+                           :source-map-timestamp true
+                           :preloads [devtools.preload]}}
                "release"
                {:source-paths ["src"]
                 :compiler {:main tile-game.grid
