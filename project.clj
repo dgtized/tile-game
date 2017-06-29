305bb8ca6f34ccbf2fa8ada7192156643a943277
Charles Comstock <dgtized@gmail.com>
2017-06-28 23:52:13 -0500

    clojurescript 1.9.660

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :min-lein-version "2.7.1"
   :main tile-game.core
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.562"]
+                 [org.clojure/clojurescript "1.9.660"]
                  [org.clojure/core.async "0.3.443"]
                  [reagent "0.6.2"]]
   :plugins [[lein-figwheel "0.5.10"]
