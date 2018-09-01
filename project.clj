95944b2bd3d037a2330275033087348c69095d99
Charles Comstock <dgtized@gmail.com>
2018-09-01 13:50:10 -0500

    clojurescript 1.10, reagent 0.8.1, and figwheel 0.5.16

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :min-lein-version "2.7.1"
   :main tile-game.core
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/core.async "0.4.474"]
-                 [reagent "0.7.0"]]
+                 [reagent "0.8.1"]]
   :plugins [[lein-figwheel "0.5.13"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
             [test2junit "1.3.3"]]
@@ -14,7 +14,7 @@
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [com.cemerick/piggieback "0.2.2"]
                                   [org.clojure/test.check "0.9.0" :scope "test"]
-                                  [figwheel-sidecar "0.5.15"]]
+                                  [figwheel-sidecar "0.5.16"]]
                    :source-paths ["src" "dev"] }}
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :clean-targets ^{:protect false}
