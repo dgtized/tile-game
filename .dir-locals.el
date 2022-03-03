fda924be2e1ac416552e7ac6985d9f7594af2aef
Charles Comstock <dgtized@gmail.com>
2022-03-02 21:24:46 -0600

    Add .dir-locals to remove prompts for build-tool/cljs-repl

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil
+  (cider-preferred-build-tool . clojure-cli)
+  (cider-clojure-cli-aliases . "dev")
+  (cider-default-cljs-repl . figwheel-main)
+  (cider-figwheel-main-default-options . ":dev")))
